73f6cd2650fa9443685f7b7f5b859f357924e4c7
PierrickHOUEE <houee.pierrick@gmail.com>
2022-10-24 18:05:34 +0200

    WIP

--- src/pages/summary/SummaryForm.tsx
+++ src/pages/summary/SummaryForm.tsx
@@ -1,5 +1,41 @@
 import * as React from "react";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
-export default function SummarayForm() {
-  return <div>Connard</div>;
+export default function SummaryForm() {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <span style={{ color: "blue" }}>Terms and Conditions</span>
+    </span>
+  );
+
+  const popover = (
+    <Popover id="popover-basic">
+        <Popover.Header></Popover>
+    </Popover>
+  )
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => {
+            setTcChecked(e.target.checked);
+          }}
+          label={checkboxLabel}
+        ></Form.Check>
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 }
--- src/pages/summary/test/SummaryForm.test.tsx
+++ src/pages/summary/test/SummaryForm.test.tsx
@@ -10,20 +10,61 @@
 
 import { render, screen } from "@testing-library/react";
 import * as React from "react";
-import SummarayForm from "../SummaryForm";
+import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
-test("Checkbox enabling and disabling button", () => {
-  render(<SummarayForm></SummarayForm>);
+test("Checkbox enabling and disabling button", async () => {
+  // Setting-up user from userEvent
+  const user = userEvent.setup();
+
+  render(<SummaryForm></SummaryForm>);
 
   // define button and checkbox
+  const button = screen.getByRole("button", { name: /confirm order/i });
+  const checkbox = screen.getByRole("checkbox", {
+    // text insensitive of case
+    name: /terms and conditions/i,
+  });
 
   // Checkbox is unchecked by default
+  expect(checkbox).not.toBeChecked();
 
   // Clicking on checkbox
+  await user.click(checkbox);
 
   // Checking checkbox enables button
+  expect(button).toBeEnabled();
 
   // Clicking on checkbox
+  await user.click(checkbox);
 
   // Unchecking checkbox again disables button
+  expect(button).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm></SummaryForm>);
+
+  // popover starts out hidden
+  // unlike getBy, which, if doesn't find creates an error,
+  // queryBy will be null
+  const nullPopover = screen.queryByText(
+    /no ice cream will be actually delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // propover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  // Hovers them
+  await user.hover(termsAndConditions);
+  // Need to research for popover -
+  // this time we can use Get as it should be in the document
+  const popover = screen.getByText(/no ice cream will be actually delivered/i);
+  // Check if popover exists
+  expect(popover).toBeInTheDocument();
+
+  // poposver disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
